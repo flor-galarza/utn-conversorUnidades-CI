ba26518d1505f3b10ad9f6b800a6dc367ad0b9f5
flor-galarza <flor.univ2024@gmail.com>
2025-06-10 17:32:35 -0300

    SCRUM-8 Hacer fallar la build

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -1,12 +1,7 @@
 export function celsiusToFahrenheit(c: number): number {
-	return (c * 9) / 5 + 32; 
+	return (c * 9) / 5 + 30; 
 }
 
 export function fahrenheitToCelsius(f: number): number {
 	return ((f - 32) * 5) / 9;
 }
-// export function celsiusToKelvin(c: number): number {
-//     return c + 273.15;
-// }
-// export function kelvinToCelsius(k: number): number {
-//     return k - 273.15;
